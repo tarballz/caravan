11c55518b01ea651c28c95692ab6ee623bae327c
Gabriel Larwood <glarwood@ucsc.edu>
2017-10-18 15:28:55 -0700

    Redirect users to google maps in navigation mode.

--- app/src/main/java/edu/cmps121/app/MapsActivity.java
+++ app/src/main/java/edu/cmps121/app/MapsActivity.java
@@ -13,11 +13,13 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_party);
+        setContentView(R.layout.activity_maps);
     }
 
     public void notifyFood(View view) {
-
+        /** Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194?q=restaurants"); searches all
+         * restaurants near those coordinates
+         */
     }
 
     public void notifyGas(View view) {
@@ -33,29 +35,11 @@ public class MapsActivity extends AppCompatActivity {
     }
 
     public void mapsRedirect(View view) {
-//        String destination = "com.google.android.apps.maps";
-//        Intent intent = getPackageManager().getLaunchIntentForPackage(destination);
-//        if (intent != null) {
-//            // We found the activity now start the activity
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            startActivity(intent);
-//        } else {
-//            // Bring user to the market or let them choose an app?
-//            intent = new Intent(Intent.ACTION_VIEW);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            intent.setData(Uri.parse("market://details?id=" + destination));
-//            startActivity(intent);
-//        }
+         Uri gmmIntentUri = Uri.parse("google.navigation:q=Taronga+Zoo,+Sydney+Australia");
 
-        // Create a Uri from an intent string. Use the result to create an Intent.
-        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
-
-        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        // Make the Intent explicit by setting the Google Maps package
         mapIntent.setPackage("com.google.android.apps.maps");
 
-        // Attempt to start an activity that can handle the Intent
         if (mapIntent.resolveActivity(getPackageManager()) != null)
             startActivity(mapIntent);
         else
